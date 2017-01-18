51d0feafbfbb8183578bf429c5381cd8a04db564
Peter Prib <peter.prib>
2017-01-18 12:04:39 +0000

    add tets

---
+++ test/base.js
@@ -0,0 +1,9 @@
+/*eslint-env node, assert, mocha, mocha*/
+var assert = require("assert");
+describe("test", function() {
+  describe("test l1", function() {
+    it("should return -1 when the value is not present", function() {
+      assert.equal(-1, [1,2,3].indexOf(4));
+    });
+  });
+});
