45195c6b1727b5e0b3dc5329040f3ff826ec5f98
Peter Prib <peter.prib>
2017-01-19 07:13:04 +0000

    teset script

--- test/base.js
+++ test/base.js
@@ -1,7 +1,7 @@
 /*eslint-env node, assert, mocha, mocha*/
 var assert = require("assert");
 describe("jsPribExtensions", function() {
-		require("./index");
+		require("../index");
 		describe("test l1", function() {
 				it("should return -1 when the value is not present", function() {
 				assert.equal(-1, [1,2,3].indexOf(4));
