9e9938f995e746dccdb36c72855316559db57a0e
Peter Prib <peter.prib>
2017-01-19 22:51:27 +0000

    tests

--- test/base.js
+++ test/base.js
@@ -1,12 +1,10 @@
 /*eslint-env node, assert, mocha, mocha*/
+/*
 var chai = require("chai");
 var expect = chai.expect;
 var assert = chai.assert;
 var should = chai.should;
-
-
 var jsPribExtensions = require("../index");
-
 describe("jsPribExtensions", function() {
 		describe("require loaded", function() {
 				it("load successfully", function() {
@@ -18,3 +16,4 @@ describe("jsPribExtensions", function() {
 					});
 		});
 	});
+*/
