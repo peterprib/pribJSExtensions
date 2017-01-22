4653cdd84f0c7ae12ff164685b911ce57482a450
Peter Prib <peter.prib>
2017-01-22 20:57:13 +0000

    a

--- test/base.js
+++ test/base.js
@@ -3,45 +3,53 @@ var chai = require("chai");
 var expect = chai.expect;
 var assert = chai.assert;
 var should = chai.should;
+					console.log("assert " +Object.getOwnPropertyNames(assert).sort());
+					console.log("expect " +Object.getOwnPropertyNames(expect).sort());
+					console.log("expect " +Object.getOwnPropertyNames(should).sort());
+	
 
-var propsArray=Object.getOwnPropertyNames(Array).sort();
-var propsObject=Object.getOwnPropertyNames(Object).sort();
-var propsString=Object.getOwnPropertyNames(String).sort();
+
+var propsArray=Object.getOwnPropertyNames([]).sort();
+var propsObject=Object.getOwnPropertyNames({}).sort();
+var propsString=Object.getOwnPropertyNames("").sort();
 console.log("Array properties: "+propsArray);
 console.log("Object properties: "+propsObject);
 console.log("String properties: "+propsString);
 
 require("../index");
 
-var propsArrayAfter=Object.getOwnPropertyNames(Array).sort();
-var propsObjectAfter=Object.getOwnPropertyNames(Object).sort();
-var propsStringAfter=Object.getOwnPropertyNames(String).sort();
+var propsArrayAfter=Object.getOwnPropertyNames([]).sort();
+var propsObjectAfter=Object.getOwnPropertyNames({}).sort();
+var propsStringAfter=Object.getOwnPropertyNames("").sort();
 
 console.log("Array properties: "+propsArrayAfter);
 console.log("Object properties: "+propsObjectAfter);
 console.log("String properties: "+propsStringAfter);
 
-console.log("Array diff: "+propsArrayAfter.outerSet(propsArray));
+console.log("Array diff: "+propsArrayAfter.outerSet([propsArray]));
 console.log("Array diff: "+propsArray.outerSet(propsArrayAfter));
 console.log("Object diff: "+propsObjectAfter.outerSet(propsObject));
 console.log("String diff: "+propsStringAfter.outerSet(propsString));
 
 
 describe("jsPribExtensions", function() {
-//		describe("require loaded", function() {
-				it("Array outerSet", function() {
-						assert.typeOf([].outerSet([]), "Array", "Array type not returned");
-						expect( [].outerSet([]) ).to.equal( [] ,"can't handle empty arras");
-						expect( [1].outerSet([]) ).to.equal( [1] ,"can't handle diff");
-						expect( [1].outerSet([1,2]) ).to.equal( [2] ,"can't handle diff");
-						expect( [1,2].outerSet([1]) ).to.equal( [2] ,"can't handle diff");
-						expect( [1,2].outerSet([1],[3]) ).to.equal( [2,3] ,"can't handle diff");
-					});
-				it("String startWith", function() {
-						assert.typeOf("atest".startWith("ad"), "boolean", "boolean type not returned");
-						expect( "atest".startWith("a") ).to.equal(true,"can't handle single char");
-						expect( "atest".startWith("at") ).to.equal(true,"can't handle multi char");
-						expect( "atest".startWith("ad") ).to.equal(false,"can't false");
-					});
-//		});
+			it("Array outerSet", function() {
+					console.log("assert " +Object.getOwnPropertyNames(assert).sort());
+					console.log("expect " +Object.getOwnPropertyNames(expect).sort());
+				
+				
+					assert.typeOf([].outerSet([]), "Array", "Array type not returned");
+					
+					expect( [].outerSet([]) ).to.equal( [] ,"can't handle empty arras");
+					expect( [1].outerSet([]) ).to.equal( [1] ,"can't handle diff");
+					expect( [1].outerSet([1,2]) ).to.equal( [2] ,"can't handle diff");
+					expect( [1,2].outerSet([1]) ).to.equal( [2] ,"can't handle diff");
+					expect( [1,2].outerSet([1],[3]) ).to.equal( [2,3] ,"can't handle diff");
+				});
+			it("String startWith", function() {
+					assert.typeOf("atest".startWith("ad"), "boolean", "boolean type not returned");
+					expect( "atest".startWith("a") ).to.equal(true,"can't handle single char");
+					expect( "atest".startWith("at") ).to.equal(true,"can't handle multi char");
+					expect( "atest".startWith("ad") ).to.equal(false,"can't false");
+				});
 	});
