8710c4a0cbcc8fda65dd88804785b1a487727a49
Peter Prib <peter.prib>
2017-01-22 06:37:00 +0000

    a

--- test/base.js
+++ test/base.js
@@ -10,10 +10,13 @@ var propsString=Object.getOwnPropertyNames(String).sort();
 console.log("Array properties: "+propsArray);
 console.log("Object properties: "+propsObject);
 console.log("String properties: "+propsString);
-var jsPribExtensions = require("../index");
+
+require("../index");
+
 var propsArrayAfter=Object.getOwnPropertyNames(Array).sort();
 var propsObjectAfter=Object.getOwnPropertyNames(Object).sort();
 var propsStringAfter=Object.getOwnPropertyNames(String).sort();
+
 console.log("Array properties: "+propsArrayAfter);
 console.log("Object properties: "+propsObjectAfter);
 console.log("String properties: "+propsStringAfter);
@@ -26,9 +29,6 @@ console.log("String diff: "+propsStringAfter.outerSet(propsString));
 
 describe("jsPribExtensions", function() {
 		describe("require loaded", function() {
-				it("load successfully", function() {
-						should.exist(jsPribExtensions,"jsPribExtensions not defined");
-					});
 				it("Array outerSet", function() {
 						assert.typeOf([].outerSet([]), "Array", "Array type not returned");
 						[].outerSet([]).should.be.equal.to([],"can't handle empty arras");
